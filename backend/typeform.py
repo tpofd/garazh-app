0c3e36fc904486da876a9a8878db0b9ee3ea0176
Anastasia Gisina <anastasia.gisina@inbox.ru>
2021-03-28 02:06:40 +0300

    create final table from typeforms

--- backend/typeform.py
+++ backend/typeform.py
@@ -1,16 +1,14 @@
 from typeform import Typeform
 import json
+import pandas as pd
 
-typeform = Typeform('FfLZwidfYeoAbHjamv3QCUpDimgA9oXFg6KUWhBS6yyz')
+# обрабатываем ответы
 responses = Typeform('FfLZwidfYeoAbHjamv3QCUpDimgA9oXFg6KUWhBS6yyz').responses
 result: dict = responses.list('or3ePC3s')
 
 result = str(result).replace("'", '"')
 
 json_object = json.dumps(result)
-
-with open('data.txt', 'w') as outfile:
-    json.dump(result, outfile)
 
 f = open("main.json", "w")
 f.write(result)
@@ -30,3 +28,35 @@ for j in range(len(data['items'])):
       answers.append(data['items'][j]['answers'][i]['text'])
   new_dict = dict(zip(questions_id, answers))
   print(new_dict)
+
+# обрабатываем вопросы
+
+forms = Typeform('FfLZwidfYeoAbHjamv3QCUpDimgA9oXFg6KUWhBS6yyz').forms
+result: dict = forms.get('or3ePC3s')
+
+result = str(result).replace("'", '"')
+result = str(result).replace('True', '"True"')
+result = str(result).replace('False', '"False"')
+
+json_object = json.dumps(result)
+
+f = open("main.json", "w")
+f.write(result)
+f.close()
+
+f = open('main.json')
+data = json.load(f)
+
+id = []
+title = []
+for i in range(len(data['fields'])):
+  id.append(data['fields'][i]['id'])
+  title.append(data['fields'][i]['title'])
+
+question_titles = dict(zip(id, title))
+print(question_titles)
+
+a = pd.DataFrame.from_dict(new_dict, orient='index')
+b = pd.DataFrame.from_dict(question_titles, orient='index')
+
+final_table = a.merge(b, left_index=True, right_index=True)
